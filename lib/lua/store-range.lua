09359b1ddd499245f69fc8faae38e637bfcb17cd
salsorrentino <sal@sorrentinobasses.com>
2017-12-16 06:42:34 -0600

    db config

--- lib/lua/store-range.lua
+++ lib/lua/store-range.lua
@@ -5,4 +5,22 @@ local len = #range
 for i=1,len do
 	redis.call('SADD', key, range[i])
 end
+
+
+
+-- local function inverse(val) 
+-- 	if val == '+inf' then return '-inf' end
+-- 	if val == '-inf' then return '+inf' end
+-- 	return '('..tostring(val)
+-- end
+
+-- redis.call('ZINTERSTORE', key, 1, KEYS[1], 'WEIGHTS', 1);
+
+-- if ARGV[2] == '+inf' then 
+-- 	redis.call('ZREMRANGEBYSCORE', key, '-inf', inverse(ARGV[1]))
+-- elseif ARGV[1] == '-inf' then
+-- 	redis.call('ZREMRANGEBYSCORE', key, inverse(ARGV[2]), '+inf')
+-- end
+
+
 return len
