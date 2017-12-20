8b1dd6faa972798f31574fbc2a3b4e37371d4a41
salsorrentino <sal@sorrentinobasses.com>
2017-12-20 06:31:47 -0600

    converting document and id stores to hash (saving keys)

--- lib/lua/existing-id.lua
+++ lib/lua/existing-id.lua
@@ -1,4 +1,4 @@
 local map,id = KEYS[1],ARGV[1]
 
-return redis.call('ZSCORE', map, id);
+return redis.call('HGET', map, id);
 
---
+++ lib/lua/get-docs.lua
@@ -0,0 +1,23 @@
+local tosort,hash,start,stop,setstore,res = KEYS[1],KEYS[2],(ARGV[1] or 0),(ARGV[2] or -1),ARGV[3],{}
+local tempStore = '__XX:SPREDIS:DOC:TEMP:XX__';
+
+
+
+redis.call('SORT', tosort, 'BY', 'nosort', 'GET', '#', 'LIMIT', start, stop, 'STORE', tempStore)
+
+local val = redis.call('LPOP', tempStore);
+
+while val do
+	if setstore then 
+		redis.call('SADD', setstore, val)
+	end
+	table.insert(res, redis.call('HGET', hash, val))
+	val = redis.call('LPOP', tempStore);
+end
+
+redis.call('DEL', tempStore);
+
+return res;
+
+
+
