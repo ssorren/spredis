8e26f4f79040cacb81f4111e306c295bdadcc9aa
salsorrentino <sal@sorrentinobasses.com>
2018-02-27 05:34:12 -0600

    adding reindex script

--- reindex.js
+++ reindex.js
@@ -1,6 +1,9 @@
-const swoosh = require('./index')({});
-swoosh.useNamespace('revsale').then(ns=> {
-	ns.fullReIndex().then(()=> {
-		console.log('re-indexing complete');
-	})
-});
+const spredis = require('./index')({});
+spredis.initialize().then( () => {
+	
+	spredis.useNamespace('revsale').then(ns=> {
+		ns.fullReIndex().then(()=> {
+			console.log('re-indexing complete');
+		})
+	});	
+} )
