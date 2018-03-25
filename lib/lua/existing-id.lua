305f02ddd27f75d8564a497a3dd30fc18366a7ee
salsorrentino <sal@sorrentinobasses.com>
2018-03-25 08:07:09 -0500

    finally dropping lua scripts, preparing for multi-language indexing

--- lib/lua/existing-id.lua
+++
@@ -1,5 +0,0 @@
--- redis.replicate_commands()
-local map,id = KEYS[1],ARGV[1]
-
-return redis.call('HGET', map, id);
-
--- lib/lua/total-found.lua
+++
@@ -1,7 +0,0 @@
--- redis.replicate_commands()
-local t = redis.call('TYPE', KEYS[1])
-t = t.ok or t
-if t == 'zset' then return redis.call('ZCARD', KEYS[1]) end
-if t == 'set' then return redis.call('SCARD', KEYS[1]) end
-if t == 'list' then return redis.call('LLEN', KEYS[1]) end
-return 0
