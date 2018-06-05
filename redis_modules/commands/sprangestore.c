4d619642599ed34f5817d2277de1c3cc1df72c5b
salsorrentino <sal@sorrentinobasses.com>
2018-06-04 19:10:25 -0500

    debugging prefix search

--- redis_modules/commands/sprangestore.c
+++ redis_modules/commands/sprangestore.c
@@ -469,11 +469,11 @@ RedisModuleString * SP_RESOLVE_WILDCARD(RedisModuleCtx *ctx, RedisModuleString *
 
     SpredisLog(ctx, "%s, len=%zu\n", str, len);
     if(len > 4 && !strcmp(str + len - 4, "\\xff")) {
-        SpredisLog(ctx, "have a wild card%s, len=%zu\n", str, len);
+        RedisModule_Log(ctx, "notice", "have a wild card%s, len=%zu\n", str, len);
         char *new_str = RedisModule_Calloc(len - 3, sizeof(char));
         strncpy(new_str, str, len - 4);
         string = RedisModule_CreateStringPrintf(ctx, "%s%c", new_str ,0xff);
-        SpredisLog(ctx, "have a wild card %s\n", new_str);
+        RedisModule_Log(ctx, "notice", "have a wild card %s\n", new_str);
         RedisModule_Free(new_str);
     }
     return string;
@@ -521,7 +521,7 @@ int SpredisStoreLexRange_RedisCommandT(RedisModuleCtx *ctx, RedisModuleString **
     gtCmp = SP_ARG_MINUS_INC_EXC(gtCmp);
     ltCmp = SP_ARG_MINUS_INC_EXC(ltCmp);
 
-    printf("gtCmp = %s\n", gtCmp);
+    RedisModule_Log(ctx, "notice", "gtCmp = %s\n", gtCmp);
 
     size_t gtLen = strlen((const char *)gtCmp);
     size_t ltLen = strlen((const char *)ltCmp);
@@ -576,7 +576,7 @@ int SpredisStoreLexRange_RedisCommandT(RedisModuleCtx *ctx, RedisModuleString **
 
     kb_intervalp(LEXSET, testLex, &t, &l, &u);
     
-    SpredisLog(ctx, "interval? %d, %d\n", l == NULL, u == NULL);
+    RedisModule_Log(ctx, "notice", "interval? %d, %d\n", l == NULL, u == NULL);
     int reached = 0;
     if (l != NULL) {
         kb_itr_getp(LEXSET, testLex, l, &itr); // get an iterator pointing to the first    
@@ -586,11 +586,11 @@ int SpredisStoreLexRange_RedisCommandT(RedisModuleCtx *ctx, RedisModuleString **
     for (; kb_itr_valid(&itr); kb_itr_next(LEXSET, testLex, &itr)) { // move on
         cand = &kb_itr_key(SPScoreSetKey, &itr);
         if (cand) {
-            SpredisLog(ctx, "GT? %s\n", cand->value.asChar);
+            RedisModule_Log(ctx, "notice", "GT? %s\n", cand->value.asChar);
             if (reached || GT(memcmp(gtCmp, (const unsigned char *)cand->value.asChar, gtLen ))) {
                 SpredisLog(ctx, "GT 1 %s\n", cand->value.asChar);
                 if (LT(memcmp((const unsigned char *)cand->value.asChar,ltCmp , ltLen ))) {
-                    SpredisLog(ctx, "LT 1 %s\n", cand->value.asChar);
+                    RedisModule_Log(ctx, "notice", "LT 1 %s\n", cand->value.asChar);
                     reached = 1;
                     SPAddAllToSet(res, cand, hint);
                 } else {
